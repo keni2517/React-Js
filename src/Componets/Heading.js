5b866a70dda4a792af7755629061d8d219866aa6
keni <kenikakadiya@gmail.com>
2024-03-22 09:49:26 +0530

    react

--- src/Componets/Heading.js
+++ src/Componets/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../Componets/Style.css"
 
 function Head(){
-    return <h1 style={{color:"royalblue" , border:"10px solid black"}}>This is heading tag.....</h1>
+    return <h1 style={{color:"royalblue" , border:"10px solid black"  , backgroundColor:"red"}}>This is heading tag.....</h1>
 }
 
 export default Head;
