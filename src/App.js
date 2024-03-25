95915c23ea432da4e6f1b6a9953a5dcfa470645e
keni <kenikakadiya@gmail.com>
2024-03-25 08:36:28 +0530

    reactjs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
-import './App.css';
-import Head from './Componets/Heading';
-import Para1,{Para2,Para3,Para4,Para5} from './Componets/Para';
-import Imga from './Componets/Img';
-import Order from './Componets/Orderlist';
+// import './App.css';
+// import Head from './Componets/Heading';
+// import Para1,{Para2,Para3,Para4,Para5} from './Componets/Para';
+// import Imga from './Componets/Img';
+// import Order from './Componets/Orderlist';
+// import Apps from './Componets/tailwind'; 
+import App2  from './Componets/task1';
+
 
 function App() {
   return (
     <div className="App">
-     <Head/>
+     {/* <Head/>
      <Para1/>
      <Para2/>
      <Para3/>
@@ -16,6 +19,8 @@ function App() {
      <Para5/>
      <Imga/>
      <Order/>
+     <Apps/> */}
+     <App2/>
     </div>
   );          
 }
